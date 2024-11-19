a6be485319576aa30ce41203911288d60b8c7d80
AmiraKhaled8991 <amirakhaled8991@gmail.com>
2024-11-19 16:07:57 +0200

    create process route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from controllers import DataController, ProcessController
 from models import ResponseSignal
+from routes import ProcessRequest
 import os
 import aiofiles
 import logging
@@ -52,3 +53,29 @@ async def uploading_file(project_id: str, file: UploadFile
                         "Path": file_id
                 }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+        
+        file_id = process_request.file_id
+        chunk_siza = process_request.chunk_size
+        chunk_overlay = process_request.chunk_overlay
+
+        process_controller = ProcessController(project_id=project_id)
+        file_content = process_controller.get_file_content(file_id=file_id)
+
+        file_chunks = process_controller.process_file_content(
+                file_content=file_content,
+                file_id=file_id,
+                chunk_size=chunk_siza,
+                overlap_size=chunk_overlay
+                )
+        
+        if file_chunks is None or len(file_chunks) == 0:
+                return JSONResponse(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        content={
+                                "Signal": ResponseSignal.PROCESSING_FAILED.value
+                        }
+                )
+        return file_chunks
